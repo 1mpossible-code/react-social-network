b1afce856e309c627d4d156a66d2e185dd0c60e0
Max <linme00p@gmail.com>
2021-10-06 22:40:54 +0300

    Create base structure for social network profile page.

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <nav className="navbar">
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar;
