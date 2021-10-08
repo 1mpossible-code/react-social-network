86f187ed3ca4b2b8a68c8c9ce9759d429f04b297
Max <linme00p@gmail.com>
2021-10-08 22:06:13 +0300

    Install and use 'react-router-dom'.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Sidebar/>
-            <main className="content">
-                <Dialogs/>
-                {/*<Profile/>*/}
-            </main>
-        </div>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Sidebar/>
+                <main className="content">
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/dialogs" component={Dialogs}/>
+                </main>
+            </div>
+        </BrowserRouter>
     );
 }
 
