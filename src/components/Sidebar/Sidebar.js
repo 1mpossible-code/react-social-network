53332334796100761253b302d481d698fa8884df
Max <linme00p@gmail.com>
2021-10-08 22:18:08 +0300

    Add NavLinks to Sidebar.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,26 @@
 import React from "react";
 import classes from "./Sidebar.module.css";
+import {NavLink} from "react-router-dom";
 
 const Sidebar = () => {
     return (
         <aside>
             <nav className={classes.navbar}>
-                <div>Profile</div>
-                <div>Messages</div>
-                <div>News</div>
-                <div>Music</div>
-                <div>Settings</div>
+                <div>
+                    <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink>
+                </div>
+                <div>
+                    News
+                </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Settings
+                </div>
             </nav>
         </aside>
     )
