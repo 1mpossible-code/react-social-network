faeb17313b0b5889e0718ba638521e9028413d1d
Max <linme00p@gmail.com>
2021-10-09 17:00:14 +0300

    Create Dialogs page basic design.

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,9 +1,19 @@
 import React from "react";
+import classes from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogFrame}>
+            <div className={classes.dialogItems}>
+                <div className={classes.item + ' ' + classes.active}>Name</div>
+                <div className={classes.item}>Name2</div>
+            </div>
+            <div className={classes.chat}>
+                <div className={classes.message}>Hi!</div>
+                <div className={classes.message + ' ' + classes.right}>Yo</div>
+                <div className={classes.message}>Test message</div>
+                <div className={classes.message}>Lorem ipsum dolor sit.</div>
+            </div>
         </div>
     )
 }
