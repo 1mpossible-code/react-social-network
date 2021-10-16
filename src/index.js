58611364c34c60aee9608221a77d4fbd27d0621a
Max <linme00p@gmail.com>
2021-10-16 15:17:02 +0300

    Remove subscribing from index.js.

--- src/index.js
+++ src/index.js
@@ -6,20 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import store from "./redux/store";
 import {Provider} from "react-redux";
 
-export const renderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
-
-renderEntireTree();
-
-store.subscribe(renderEntireTree);
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
