5144b17e38a920e9ac115a99eadaa5328f8ab774
Max <linme00p@gmail.com>
2021-10-19 15:51:32 +0300

    Move logic from presentational component to container one.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,68 +1,42 @@
-import React, {Component} from "react";
+import React from "react";
 import classes from './Users.module.css';
-import axios from "axios";
 
-class Users extends Component {
-    LIMIT = 5;
-
-    componentDidMount() {
-        this.getUsersPage(1)
-    }
-
-    getUsersPage(page) {
-        axios.get(`https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${this.LIMIT}`).then(
-            res => {
-                this.props.addUsers(res.data);
-            }
-        )
-    }
-
-    loadMoreUsers() {
-        this.props.incrementCurrentPage();
-        this.getUsersPage(this.props.currentPage + 1);
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Users</h1>
-                {
-                    this.props.users.map(user => (
-                        <div key={user.id} className={classes.userContainer}>
-                            <div className={classes.container}>
-                                <div>{user.name}</div>
-                                <div className={classes.userDescription}>{user.description}</div>
-                            </div>
-                            <button
-                                className={classes.followBtn}
-                                onClick={() => {
-                                    this.props.onFollowBtnClick(user.id)
-                                }}
-                            >{user.follows ? 'Unfollow' : 'Follow'}</button>
-                        </div>
-                    ))
-                }
-
-                {
-                    ((this.props.currentPage * this.LIMIT) <= this.props.users.length) &&
-                    <div className={classes.loadBtnContainer}>
-                        <button className={classes.loadBtn} onClick={() => {
-                            this.loadMoreUsers()
-                        }}>Load More
-                        </button>
+const Users = (props) => {
+    return <div>
+        <h1>Users</h1>
+        {
+            props.users.map(user => (
+                <div key={user.id} className={classes.userContainer}>
+                    <div className={classes.container}>
+                        <div>{user.name}</div>
+                        <div className={classes.userDescription}>{user.description}</div>
                     </div>
-                }
+                    <button
+                        className={classes.followBtn}
+                        onClick={() => {
+                            props.onFollowBtnClick(user.id)
+                        }}
+                    >{user.follows ? 'Unfollow' : 'Follow'}</button>
+                </div>
+            ))
+        }
 
-                {
-                    ((this.props.currentPage * this.LIMIT) === this.props.users.length + this.LIMIT) &&
-                    <div className={classes.loadBtnContainer}>
-                        <span>that's all</span>
-                    </div>
-                }
-
+        {
+            ((props.currentPage * props.LIMIT) <= props.users.length) &&
+            <div className={classes.loadBtnContainer}>
+                <button className={classes.loadBtn} onClick={props.loadMoreUsers}>Load More
+                </button>
             </div>
-        );
-    }
+        }
+
+        {
+            ((props.currentPage * props.LIMIT) === props.users.length + props.LIMIT) &&
+            <div className={classes.loadBtnContainer}>
+                <span>that's all</span>
+            </div>
+        }
+
+    </div>
 }
 
 export default Users;
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -5,6 +5,41 @@ import {
 } from "../../redux/usersReducer";
 import {connect} from "react-redux";
 import Users from "./Users";
+import {Component} from "react";
+import axios from "axios";
+
+class UsersContainer extends Component {
+    LIMIT = 4;
+
+    componentDidMount() {
+        this.getUsersPage(1)
+    }
+
+    getUsersPage(page) {
+        axios.get(`https://jsonplaceholder.typicode.com/users?_page=${page}&_limit=${this.LIMIT}`).then(
+            res => {
+                this.props.addUsers(res.data);
+            }
+        )
+    }
+
+    loadMoreUsers() {
+        this.props.incrementCurrentPage();
+        this.getUsersPage(this.props.currentPage + 1);
+    }
+
+    render() {
+        return (
+            <Users
+                users={this.props.users}
+                onFollowBtnClick={this.props.onFollowBtnClick}
+                currentPage={this.props.currentPage}
+                loadMoreUsers={() => {this.loadMoreUsers()}}
+                LIMIT={this.LIMIT}
+            />
+        );
+    }
+}
 
 const mapStateToProps = (state) => ({
     users: state.usersPage.users,
@@ -23,4 +58,4 @@ const mapDispatchToProps = (dispatch) => ({
     }
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
