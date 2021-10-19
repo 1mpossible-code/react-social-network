a0e579ab20cf705853e31d5b84b245bc077e3b78
Max <linme00p@gmail.com>
2021-10-19 14:22:21 +0300

    Replace functional component with class.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,38 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import classes from './Users.module.css';
 import axios from "axios";
 
-const Users = (props) => {
-    if (props.users.length === 0) {
+class Users extends Component {
+    componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/users').then(
             res => {
-                props.setUsers(res.data);
-                console.log(res.data)
+                this.props.setUsers(res.data);
             }
         )
     }
 
-    return (
-        <div>
-            <h1>Users</h1>
-            {
-                props.users.map(user => (
-                    <div key={user.id} className={classes.userContainer}>
-                        <div className={classes.container}>
-                            <div>{user.name}</div>
-                            <div className={classes.userDescription}>{user.description}</div>
+    render() {
+        return (
+            <div>
+                <h1>Users</h1>
+                {
+                    this.props.users.map(user => (
+                        <div key={user.id} className={classes.userContainer}>
+                            <div className={classes.container}>
+                                <div>{user.name}</div>
+                                <div className={classes.userDescription}>{user.description}</div>
+                            </div>
+                            <button
+                                className={classes.followBtn}
+                                onClick={() => {
+                                    this.props.onFollowBtnClick(user.id)
+                                }}
+                            >{user.follows ? 'Unfollow' : 'Follow'}</button>
                         </div>
-                        <button
-                            className={classes.followBtn}
-                            onClick={() => {
-                                props.onFollowBtnClick(user.id)
-                            }}
-                        >{user.follows ? 'Unfollow' : 'Follow'}</button>
-                    </div>
-                ))
-            }
-        </div>
-    )
+                    ))
+                }
+            </div>
+        );
+    }
 }
 
 export default Users;
