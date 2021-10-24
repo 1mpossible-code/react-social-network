94f472aaee8d12c75dba041ddea5135bc996e8d1
Max <linme00p@gmail.com>
2021-10-24 16:54:48 +0300

    Update withAuthRedirect HOC to wait till response from auth server.

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,12 +1,14 @@
 import userAPI from "../api/api";
 
 const SET_AUTH_USER = 'SET_AUTH_USER';
+const SET_IS_FETCHING = 'SET_IS_FETCHING';
 
 const initialState = {
     userId: null,
     name: null,
     email: null,
     isAuthorized: false,
+    isFetching: true,
 }
 
 const authReducer = (state = initialState, action) => {
@@ -16,6 +18,11 @@ const authReducer = (state = initialState, action) => {
                 ...state,
                 ...action.data,
                 isAuthorized: true,
+            }
+        case SET_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.isFetching,
             }
         default:
             return state;
@@ -27,12 +34,19 @@ export const setAuthUser = (userId, name, email) => ({
     data: {userId, name, email},
 })
 
+export const setIsFetching = (isFetching) => ({
+    type: SET_IS_FETCHING,
+    isFetching,
+})
+
 export const getAuthUserThunk = () => (dispatch) => {
+    dispatch(setIsFetching(true));
     userAPI.getAuthMe().then(
         ({_id, name, email}) => {
             dispatch(setAuthUser(_id, name, email));
-        }
-    ).catch(e => console.error(e));
+        })
+        .catch(e => console.error(e))
+        .finally(() => dispatch(setIsFetching(false)));
 }
 
 export default authReducer;
