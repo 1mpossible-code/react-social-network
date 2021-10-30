d85f7db434a165b5a2af07e115e22c80f3b6a803
Max <linme00p@gmail.com>
2021-10-30 21:23:38 +0300

    Create switch for Routes.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import './App.css';
 import Sidebar from "./components/Sidebar/Sidebar";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import {Component} from "react";
 import {initialize} from "./redux/appReducer";
 import {connect} from "react-redux";
 import Preloader from "./components/Utils/Preloader";
 
-// import ProfileContainer from "./components/Profile/ProfileContainer";
-// import Dialogs from "./components/Dialogs/Dialogs";
-// import UsersContainer from "./components/Users/UsersContainer";
-// import Login from "./components/Login/Login";
 const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"))
 const Dialogs = React.lazy(() => import("./components/Dialogs/Dialogs"))
 const UsersContainer = React.lazy(() => import("./components/Users/UsersContainer"))
@@ -31,16 +27,18 @@ class App extends Component {
                     <Sidebar/>
                     <main className="content">
                         <React.Suspense fallback={Preloader}>
-                            <Route path="/profile/:userId" render={() => (
-                                <ProfileContainer/>
-                            )}/>
-                            <Route path="/dialogs" render={() => (
-                                <Dialogs/>
-                            )}/>
-                            <Route path="/users" render={() => (
-                                <UsersContainer/>
-                            )}/>
-                            <Route path="/login" component={Login}/>
+                            <Switch>
+                                <Route path="/profile/:userId" render={() => (
+                                    <ProfileContainer/>
+                                )}/>
+                                <Route path="/dialogs" render={() => (
+                                    <Dialogs/>
+                                )}/>
+                                <Route path="/users" render={() => (
+                                    <UsersContainer/>
+                                )}/>
+                                <Route path="/login" component={Login}/>
+                            </Switch>
                         </React.Suspense>
                     </main>
                 </div>
