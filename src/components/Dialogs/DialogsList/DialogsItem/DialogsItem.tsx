608155560d44f023998113656aad39dc0bbf2b66
Max <linme00p@gmail.com>
2021-11-25 23:14:47 +0200

    Rewrite DialogsItem in TypeScript.

--- src/components/Dialogs/DialogsList/DialogsItem/DialogsItem.tsx
+++ src/components/Dialogs/DialogsList/DialogsItem/DialogsItem.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {FC} from "react";
 import classes from "./DialogsItem.module.css"
 
-const DialogsItem = (props) => {
+type Props = {
+    id: number;
+    name: string;
+}
+
+const DialogsItem: FC<Props> = (props) => {
     return (
         <div className={classes.dialogsItem + ' ' + classes.active}>
             {props.name}
