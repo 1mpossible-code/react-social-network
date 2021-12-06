d5d57ccc0395547fdfb50cd166365bfb54dfac89
Max <linme00p@gmail.com>
2021-12-06 22:20:33 +0200

    Rewrite HOC withAuthRedirect in TypeScript.

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {ComponentType, FC} from "react";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
+import {RootState} from "../redux/store";
 
-const withAuthRedirect = (Component) => {
-    const AuthRedirect = (props) => {
+type Props = {
+    isAuthorized: boolean;
+}
+
+const withAuthRedirect = (Component: ComponentType) => {
+    const AuthRedirect: FC<Props> = (props) => {
         if (!props.isAuthorized) return <Redirect to="/login"/>;
         return <Component {...props} />;
     }
-    const mapStateToProps = (state) => ({
+    const mapStateToProps = (state: RootState) => ({
         isAuthorized: state.auth.isAuthorized,
     })
     return connect(mapStateToProps)(AuthRedirect);
